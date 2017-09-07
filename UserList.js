c74f752b97998aaa0b222a8e516035f394531907
Brent Gardner <bgardner@squarelabs.net>
2017-09-07 11:26:35 +0900

    Load component from fragment

--- UserList.js
+++ UserList.js
@@ -1,38 +1,21 @@
 // Create a class for the element
 export default class XProduct extends HTMLElement {
     constructor() {
-      // Always call super first in constructor
-      super();
-  
-      // Create a shadow root
-      var shadow = this.attachShadow({mode: 'open'});
-  
-      // Create a standard img element and set it's attributes.
-      var img = document.createElement('img');
-      img.alt = this.getAttribute('data-name');
-      img.src = this.getAttribute('data-img');
-      img.width = '150';
-      img.height = '150';
-      img.className = 'product-img';
-  
-      // Add the image to the shadow root.
-      shadow.appendChild(img);
-  
-      // Add an event listener to the image.
-      img.addEventListener('click', () => {
-        window.location = this.getAttribute('data-url');
-      });
-  
-      // Create a link to the product.
-      var link = document.createElement('a');
-      link.innerText = this.getAttribute('data-name');
-      link.href = this.getAttribute('data-url');
-      link.className = 'product-name';
-  
-      // Add the link to the shadow root.
-      shadow.appendChild(link);
+        super();
+
+        this.onload();
     }
-  }
-  
-  // Define the new element
-  customElements.define('x-product', XProduct);
+
+    async onload() {
+        const html = await (await fetch('UserList.html')).text();
+        const doc = document.createElement( 'html' );
+        doc.innerHTML = html;
+        console.log('len=', doc.childNodes.length);
+        const el = doc.childNodes[1].childNodes[0];
+        var shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(el);
+    }
+}
+
+// Define the new element
+customElements.define('x-product', XProduct);
